5eab09fca064fc263c48b463655826761c238a2b
Patrick Lowry <patio.lowry@gmail.com>
2026-03-20 00:22:54 -0400

    require token for tmdb requests

--- backend/src/routes/tmdb.ts
+++ backend/src/routes/tmdb.ts
@@ -1,16 +1,17 @@
 import express from 'express';
 import tmdbService from '../services/tmdbService'
+import middleware from '../utils/middleware'
 
 const router = express.Router();
 
-router.get('/search/movie', async (req, res) => {
+router.get('/search/movie', middleware.userExtractor, async (req, res) => {
     const query = req.query.query as string
     const results = await tmdbService.searchMovie(query)
     res.send(results)
 })
 
-router.get('/movie/:id', async (req, res) => {
-    const id = req.params.id;
+router.get('/movie/:id', middleware.userExtractor, async (req, res) => {
+    const id = req.params.id as string;
     const movie = await tmdbService.getMovieDetails(id);
     res.send(movie)
 })
--- frontend/src/services/tmdb.ts
+++ frontend/src/services/tmdb.ts
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import { TmdbMovieDetails, TmdbSearchResults } from '../types';
+import utils from './utils'
 
 const baseUrl = '/api/tmdb';
 
 const searchMovie = async (text: string) => {
   const { data } = await axios.get<TmdbSearchResults>(
-    `${baseUrl}/search/movie?query=${encodeURIComponent(text)}`
+    `${baseUrl}/search/movie?query=${encodeURIComponent(text)}`,
+    utils.getAuthConfig()
   );
 
   return data;
@@ -13,7 +15,8 @@ const searchMovie = async (text: string) => {
 
 const getMovieDetails = async (id: number) => {
   const { data } = await axios.get<TmdbMovieDetails>(
-    `${baseUrl}/movie/${id}`
+    `${baseUrl}/movie/${id}`,
+    utils.getAuthConfig()
   );
 
   return data;
