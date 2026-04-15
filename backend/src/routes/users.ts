a04f122822ef3d0602ebaad12887ee00ea6810c6
Patrick Lowry <patio.lowry@gmail.com>
2026-04-14 21:31:45 -0400

    fix build error

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,43 +1,49 @@
-import express from 'express';
-import userService from '../services/userService';
-import middleware from '../utils/middleware';
-import { UserInfo } from '../types';
-import config from '../utils/config';
-import { Response, Request } from 'express';
-import { UserInfo } from '../types';
+import express from "express";
+import userService from "../services/userService";
+import middleware from "../utils/middleware";
+import { UserInfo } from "../types";
+import config from "../utils/config";
+import { Response, Request } from "express";
 
 const router = express.Router();
 
-router.post('/', middleware.userExtractor, async (req: Request<unknown, unknown, UserInfo>, res: Response) => {
+router.post(
+  "/",
+  middleware.userExtractor,
+  async (req: Request<unknown, unknown, UserInfo>, res: Response) => {
     const loggedUser = req.user as UserInfo | undefined;
     if (!loggedUser || loggedUser.username !== config.ADMIN_USER) {
-        res.status(401).send({ "error": "Only admins can create new users"});
-        return;
+      res.status(401).send({ error: "Only admins can create new users" });
+      return;
     }
 
     try {
-        const user = await userService.createUser(req.body);
-        res.json(user);
+      const user = await userService.createUser(req.body);
+      res.json(user);
     } catch (error: unknown) {
-        let errorMessage = 'Something went wrong.';
-        if (error instanceof Error) {
-        errorMessage += ' Error: ' + error.message;
-        }
-        res.status(400).send({ "error": errorMessage});
+      let errorMessage = "Something went wrong.";
+      if (error instanceof Error) {
+        errorMessage += " Error: " + error.message;
+      }
+      res.status(400).send({ error: errorMessage });
     }
-});
+  },
+);
 
-router.post('/login', async (req: Request<unknown, unknown, UserInfo>, res: Response) => {
+router.post(
+  "/login",
+  async (req: Request<unknown, unknown, UserInfo>, res: Response) => {
     try {
-        const user = await userService.login(req.body);
-        res.json(user);
+      const user = await userService.login(req.body);
+      res.json(user);
     } catch (error: unknown) {
-        let errorMessage = 'Something went wrong.';
-        if (error instanceof Error) {
-        errorMessage += ' Error: ' + error.message;
-        }
-        res.status(400).send({ "error": errorMessage});
+      let errorMessage = "Something went wrong.";
+      if (error instanceof Error) {
+        errorMessage += " Error: " + error.message;
+      }
+      res.status(400).send({ error: errorMessage });
     }
-});
+  },
+);
 
 export default router;
