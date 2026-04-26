ec1cfe44886cbca5f23baacbc0a017fdf3604a1e
Patrick Lowry <patio.lowry@gmail.com>
2026-04-26 15:24:08 -0400

    update frontend services to optionally pull full movie details

--- frontend/src/services/history.ts
+++ frontend/src/services/history.ts
@@ -1,47 +1,49 @@
-import axios from 'axios';
-import utils from './utils'
-import { LogEntry, NewLogEntry } from '../types'
+import axios from "axios";
+import utils from "./utils";
+import { LogEntry, NewLogEntry } from "../types";
 
 const baseUrl = `${utils.backendUrl()}/history`;
 
-const getAllEntries = async () => {
-  const { data } = await axios.get<LogEntry[]>(baseUrl)
+const getAllEntries = async (detailed: boolean = false) => {
+  const { data } = await axios.get<LogEntry[]>(baseUrl, {
+    params: { detailed },
+  });
 
   return data;
-}
+};
 
 const addEntry = async (entry: NewLogEntry) => {
-    const { data } = await axios.post<LogEntry>(
-        baseUrl,
-        entry,
-        utils.getAuthConfig()
-    )
+  const { data } = await axios.post<LogEntry>(
+    baseUrl,
+    entry,
+    utils.getAuthConfig(),
+  );
 
-    return data
-}
+  return data;
+};
 
 const updateEntry = async (id: string, entry: NewLogEntry) => {
-    const { data } = await axios.put<LogEntry>(
-        `${baseUrl}/${id}`,
-        entry,
-        utils.getAuthConfig()
-    )
+  const { data } = await axios.put<LogEntry>(
+    `${baseUrl}/${id}`,
+    entry,
+    utils.getAuthConfig(),
+  );
 
-    return data
-}
+  return data;
+};
 
 const deleteEntry = async (id: string) => {
-    const { data } = await axios.delete(
-        `${baseUrl}/${id}`,
-        utils.getAuthConfig()
-    )
+  const { data } = await axios.delete(
+    `${baseUrl}/${id}`,
+    utils.getAuthConfig(),
+  );
 
-    return data
-}
+  return data;
+};
 
 export default {
-    getAllEntries,
-    addEntry,
-    updateEntry,
-    deleteEntry
-}
+  getAllEntries,
+  addEntry,
+  updateEntry,
+  deleteEntry,
+};
--- frontend/src/services/treasures.ts
+++ frontend/src/services/treasures.ts
@@ -1,47 +1,49 @@
-import axios from 'axios';
-import utils from './utils'
-import { Treasure, NewTreasure } from '../types'
+import axios from "axios";
+import utils from "./utils";
+import { Treasure, NewTreasure } from "../types";
 
 const baseUrl = `${utils.backendUrl()}/treasures`;
 
-const getAll = async () => {
-  const { data } = await axios.get<Treasure[]>(baseUrl)
+const getAll = async (detailed: boolean = false) => {
+  const { data } = await axios.get<Treasure[]>(baseUrl, {
+    params: { detailed },
+  });
 
   return data;
-}
+};
 
 const addTreasure = async (treasure: NewTreasure) => {
-    const { data } = await axios.post<Treasure>(
-        baseUrl,
-        treasure,
-        utils.getAuthConfig()
-    )
+  const { data } = await axios.post<Treasure>(
+    baseUrl,
+    treasure,
+    utils.getAuthConfig(),
+  );
 
-    return data
-}
+  return data;
+};
 
 const updateTreasure = async (id: string, treasure: NewTreasure) => {
-    const { data } = await axios.put<Treasure>(
-        `${baseUrl}/${id}`,
-        treasure,
-        utils.getAuthConfig()
-    )
+  const { data } = await axios.put<Treasure>(
+    `${baseUrl}/${id}`,
+    treasure,
+    utils.getAuthConfig(),
+  );
 
-    return data
-}
+  return data;
+};
 
 const deleteTreasure = async (id: string) => {
-    const { data } = await axios.delete(
-        `${baseUrl}/${id}`,
-        utils.getAuthConfig()
-    )
+  const { data } = await axios.delete(
+    `${baseUrl}/${id}`,
+    utils.getAuthConfig(),
+  );
 
-    return data
-}
+  return data;
+};
 
 export default {
-    getAll,
-    addTreasure,
-    updateTreasure,
-    deleteTreasure
-}
+  getAll,
+  addTreasure,
+  updateTreasure,
+  deleteTreasure,
+};
